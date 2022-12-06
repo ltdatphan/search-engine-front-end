5eac2c7ff1a858da0ff7adb62b43507c1539024f
David Phan <david@Davids-MacBook-Pro.local>
2022-12-05 21:22:20 -0500

    updated loading message

--- src/components/Spinner.jsx
+++ src/components/Spinner.jsx
@@ -9,6 +9,10 @@ const Spinner = () => {
         please send your query again. It is likely the server hasn't been booted
         up when the query was sent.
       </span>
+      <span>
+        Please allow 20-30 seconds for the server to fully boot up. Then,
+        refresh the page and send your query!
+      </span>
       <div className="lds-grid">
         <div></div>
         <div></div>
