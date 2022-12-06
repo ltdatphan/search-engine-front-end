f95100dea4522f8152213e1c84639388de98c113
David Phan <david@Davids-MacBook-Pro.local>
2022-12-05 21:15:14 -0500

    updated loader to include message for user

--- src/components/Spinner.jsx
+++ src/components/Spinner.jsx
@@ -4,6 +4,11 @@ import "./stylesheets/spinner.css";
 const Spinner = () => {
   return (
     <div className="spinner-container">
+      <span>
+        Loading resources. If this has been loading for more than 10 seconds,
+        please send your query again. It is likely the server hasn't been booted
+        up when the query was sent.
+      </span>
       <div className="lds-grid">
         <div></div>
         <div></div>
