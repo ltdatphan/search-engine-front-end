7f1df6519df35c87d31eda065f9b98c24c49f75c
David Phan <29266892+ltdatphan@users.noreply.github.com>
2022-12-17 15:31:08 -0500

    added graphing

--- src/api/api.js
+++ src/api/api.js
@@ -5,3 +5,7 @@ const baseURL = process.env.REACT_APP_API_URL;
 export const search = (query = "") => {
   return axios.get(`${baseURL}/search?query=${query}`);
 };
+
+export const search_scores = (query = "") => {
+  return axios.get(`${baseURL}/search_scores?query=${query}`);
+};
---
+++ src/components/BarChart.jsx
@@ -0,0 +1,52 @@
+import React, { useState, useEffect } from "react";
+import Chart from "chart.js/auto";
+import { Bar } from "react-chartjs-2";
+
+const BarChart = ({
+  xData,
+  yData,
+  title,
+  legend,
+  yAxisLabel,
+  xAxisLabel,
+  backgroundColor,
+}) => {
+  return (
+    <div
+      style={{
+        padding: "30px",
+        display: "flex",
+        justifyContent: "center",
+        flexDirection: "column",
+      }}
+    >
+      <Bar
+        data={{
+          labels: xData,
+          datasets: [
+            {
+              label: legend,
+              backgroundColor: backgroundColor,
+              //   borderColor: "rgb(0, 99, 132)",
+              data: yData,
+            },
+          ],
+        }}
+        options={{
+          plugins: {
+            title: {
+              display: true,
+              text: title,
+            },
+          },
+        }}
+      />
+      <span style={{ textAlign: "center" }}>
+        X-axis: {xAxisLabel} &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; Y-axis:{" "}
+        {yAxisLabel}
+      </span>
+    </div>
+  );
+};
+
+export default BarChart;
